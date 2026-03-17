0fa1422387111e76975f1a542def761fa773cc60
avalos099-ai <avalos099@gmail.com>
2026-03-16 22:55:35 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'evaluador-v1';
+const assets = [
+  'index.html',
+  'manifest.json'
+];
+
+// Instalar el Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Responder desde la memoria (caché) para que sea veloz
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
